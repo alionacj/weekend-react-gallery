2e4dade3c16234071cb9609db0208be8f6535e89
Leo Johnson <leochinonejohnson@gmail.com>
2023-11-22 16:13:57 -0600

    created sql db and get route

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
@@ -8,7 +9,19 @@ router.put('/like/:id', (req, res) => {
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlQueryText =
+    `
+      SELECT * FROM gallery
+      ORDER BY "id";
+    `
+  pool.query(sqlQueryText)
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((error) => {
+      console.error(error)
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
